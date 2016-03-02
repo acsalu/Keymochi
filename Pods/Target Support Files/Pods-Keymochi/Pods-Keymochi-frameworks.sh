72730ffe70c1242ca8809423b23ef019e4de64f8
Huai-Che (Acsa) Lu <hc.acsalu@gmail.com>
2016-03-02 15:03:23 -0500

    Add Realm and SwiftDate pods.

--- Pods/Target Support Files/Pods-Keymochi/Pods-Keymochi-frameworks.sh
+++ Pods/Target Support Files/Pods-Keymochi/Pods-Keymochi-frameworks.sh
@@ -82,3 +82,14 @@ strip_invalid_archs() {
   fi
 }
 
+
+if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "Pods-Keymochi/Realm.framework"
+  install_framework "Pods-Keymochi/RealmSwift.framework"
+  install_framework "Pods-Keymochi/SwiftDate.framework"
+fi
+if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "Pods-Keymochi/Realm.framework"
+  install_framework "Pods-Keymochi/RealmSwift.framework"
+  install_framework "Pods-Keymochi/SwiftDate.framework"
+fi
--- Pods/Target Support Files/Pods-KeymochiTests/Pods-KeymochiTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_KeymochiTests : NSObject
-@end
-@implementation PodsDummy_Pods_KeymochiTests
-@end
